c5a02b3af87581c1f24902fdd071893e8aa63311
LatikaJadhav94 <latikaj189@gmail.com>
2022-08-12 00:51:32 +0530

    project add central repository

---
+++ SpringBootCrudWithRestfull/src/main/java/com/BikkadIt/SpringBootCrudWithRestful/SpringBootCrudWithRestfullApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIt.SpringBootCrudWithRestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudWithRestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudWithRestfullApplication.class, args);
+	}
+
+}
---
+++ SpringBootCrudWithRestfull/src/test/java/com/BikkadIt/SpringBootCrudWithRestful/SpringBootCrudWithRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIt.SpringBootCrudWithRestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudWithRestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
