931a07fe992b5c438c9687b49887b91bdebec9ce
LatikaJadhav94 <latikaj189@gmail.com>
2022-08-13 02:23:09 +0530

    SBCWR-8: Create Daoi Interface and DaoImpl class

--- SpringBootCrudWithRestfull/src/main/java/com/BikkadIt/SpringBootCrudWithRestful/dao/DaoI.java
+++ SpringBootCrudWithRestfull/src/main/java/com/BikkadIt/SpringBootCrudWithRestful/dao/DaoI.java
@@ -1,5 +1,13 @@
 package com.BikkadIt.SpringBootCrudWithRestful.dao;
 
-public class DaoI {
+import java.util.List;
 
+import com.BikkadIt.SpringBootCrudWithRestful.model.Employee;
+
+public interface DaoI {
+
+	public int addEmployee(Employee employee);
+	public List<Employee> getAllEmployee();
+	
+	
 }
--- SpringBootCrudWithRestfull/src/main/java/com/BikkadIt/SpringBootCrudWithRestful/dao/DaoImpl.java
+++ SpringBootCrudWithRestfull/src/main/java/com/BikkadIt/SpringBootCrudWithRestful/dao/DaoImpl.java
@@ -1,5 +1,38 @@
 package com.BikkadIt.SpringBootCrudWithRestful.dao;
 
-public class DaoImpl {
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.BikkadIt.SpringBootCrudWithRestful.model.Employee;
+
+@Repository
+public class DaoImpl implements DaoI{
+
+	@Autowired
+	private SessionFactory sf;
+	
+	@Override
+	public int addEmployee(Employee employee) {
+		Session session=sf.openSession();
+		session.beginTransaction();
+		int id=(int)session.save(employee);
+		System.out.println(id);
+		session.getTransaction().commit();
+		return id;
+	}
+
+	@Override
+	public List<Employee> getAllEmployee() {
+		Session session = sf.openSession();
+		String hql="from employee";
+		 Query query = session.createQuery(hql);
+		 List list = query.getResultList();
+		return list;
+	}
 
 }
